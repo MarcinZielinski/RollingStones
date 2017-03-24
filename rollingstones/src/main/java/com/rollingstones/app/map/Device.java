c0831291cca985f953b0d5e0927ae4a7cf944424
Mrz355 <zielinskimarcin123@gmail.com>
2017-03-25 00:34:44 +0100

    Sample device representantive class

---
+++ rollingstones/src/main/java/com/rollingstones/app/map/Device.java
@@ -0,0 +1,40 @@
+package com.rollingstones.app.map;
+
+/**
+ * Created by Marcin on 2017-03-24.
+ */
+public class Device {
+    private String id;
+    private double lat;
+    private double lon;
+
+    public Device(String id, double lat, double lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
