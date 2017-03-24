f192f7632e2ba87ea7f1ace5cb4313de7ebb0bfc
Somas3k <sikorka1901@gmail.com>
2017-03-24 23:36:15 +0100

    added ApplicationUtils

---
+++ rollingstones/src/main/java/com/rollingstones/app/ApplicationUtils.java
@@ -0,0 +1,10 @@
+package com.rollingstones.app;
+
+/**
+ * Created by Somas3k on 24.03.2017.
+ */
+public class ApplicationUtils {
+    public static MainApp mainApp;
+
+    private ApplicationUtils(){};
+}
